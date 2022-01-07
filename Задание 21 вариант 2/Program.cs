b4f200429a8bc847ff44e7fdd32a627dc4a7be7c
Азиза Амануллина <zizowca@gmail.com>
2022-01-08 00:30:34 +0300

    Добавьте файлы проекта.

---
+++ Задание 21 вариант 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задание_21_вариант_2
+{
+    class Program
+    {
+        private static int[,] garden;
+        private static int m;
+        private static int n;
+        static void Main(string[] args)
+        {
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+
+            garden = new int[n, m];
+
+            Thread sadov1 = new Thread(Gardner1);
+            Thread sadov2 = new Thread(Gardner2);
+
+            sadov1.Start();
+            sadov2.Start();
+
+            sadov1.Join();
+            sadov2.Join();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(garden[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+        private static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (garden[i, j] == 0)
+                        garden[i, j] = 1;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        private static void Gardner2()
+        {
+            for (int i = m - 1; i > 0; i--)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    if (garden[j, i] == 0)
+                        garden[j, i] = 2;
+                    Thread.Sleep(1);
+                }
+            }
+        }
+    }
+}
---
+++ Задание 21/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задание_21
+{
+    class Program
+    {
+        const int n = 4;
+        const int t = 6;
+        static int[,] garden = new int[n, t] { {1,4,7,3,9,5 },{ 2, 6, 7, 4, 10, 8 },{ 11, 15, 13, 13, 10, 18 },{ 14, 2, 8, 10, 3, 1 } };
+        static void Main(string[] args)
+        {
+            ThreadStart ready = new ThreadStart(Gardner1);
+            Thread steady = new Thread(ready);
+            steady.Start();
+            Gardner2();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write($"{garden[i,j]}");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    if (garden[i, j] >= 0)
+                    {
+                        int stop = garden[i, j];
+                        garden[i, j] = -1;
+                        Thread.Sleep(stop);
+                    }
+                }
+            }
+        }
+        static void Gardner2()
+        {
+            for (int i = n-1; i >=0; i--)
+            {
+                for (int j = t-1; j >= 0; j--)
+                {
+                    if (garden[i, j] >= 0)
+                    {
+                        int stop = garden[i, j];
+                        garden[i, j] = -2;
+                        Thread.Sleep(stop);
+                    }
+                }
+            }
+        }
+    }
+}
